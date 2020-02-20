21ae273fd44e135f3828e4028977d3af2bc42ff8
Ibragim Abbasov <blancduman@gmail.com>
2020-02-20 21:04:32 +0400

    feat: subtask transform array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        if (typeof arr[i + 1] !== "undefined") i++;
+        break;
+      case "--discard-prev":
+        if (typeof arr[i - 1] !== "undefined") result.pop();
+        break;
+      case "--double-next":
+        if (typeof arr[i + 1] !== "undefined") result.push(arr[i + 1]);
+        break;
+      case "--double-prev":
+        if (typeof arr[i - 1] !== "undefined") result.push(arr[i - 1]);
+        break;
+
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+
+  return result;
 };
