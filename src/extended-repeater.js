f5c53d9fcb01b50e0c40360ef56efae9e71378e5
Ibragim Abbasov <blancduman@gmail.com>
2020-02-20 22:50:32 +0400

    feat: subtask extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,34 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+const defaultOptions = {
+  repeatTimes: 1,
+  separator: "+",
+  addition: "",
+  additionRepeatTimes: 1,
+  additionSeparator: ""
 };
-  
+module.exports = function repeater(str, options) {
+  let newOptions = {
+    ...defaultOptions,
+    ...Object.keys(options).reduce((actully, key) => {
+      if (typeof options[key] !== "undefined")
+        return { ...actully, [key]: options[key] };
+      return actully;
+    }, {})
+  };
+
+  let result = "";
+
+  for (let i = 0; i < newOptions.repeatTimes; i++) {
+    result += `${str}`;
+    for (let j = 0; j < newOptions.additionRepeatTimes; j++) {
+      result += `${newOptions.addition}${
+        j !== newOptions.additionRepeatTimes - 1
+          ? newOptions.additionSeparator
+          : ""
+      }`;
+    }
+
+    if (i !== newOptions.repeatTimes - 1) result += `${newOptions.separator}`;
+  }
+
+  return result;
+};
