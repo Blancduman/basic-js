91488652ede049b1dde1f63ef23d8a1bd0024d68
Ibragim Abbasov <blancduman@gmail.com>
2020-02-20 22:50:50 +0400

    feat: subtask vigenere cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  doSomethingAlready(message, key, hacker) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let currentIndex = 0;
+    let resultLetter = "";
+    for (let i in message) {
+      if (this.alphabet.indexOf(message[i]) < 0) {
+        resultLetter += message[i];
+        continue;
+      }
+      let currentLetterIndexInAlfabet = this.alphabet.indexOf(message[i]);
+      let keyLetterIndexInAlfabet = this.alphabet.indexOf(
+        key[currentIndex % key.length]
+      );
+      resultLetter += this.alphabet[
+        hacker
+          ? (currentLetterIndexInAlfabet - keyLetterIndexInAlfabet + 26) % 26
+          : (currentLetterIndexInAlfabet + keyLetterIndexInAlfabet) % 26
+      ];
+      currentIndex += 1;
+    }
+    if (!this.direct) {
+      return resultLetter
+        .split("")
+        .reverse()
+        .join("");
+    }
+    return resultLetter;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    let encryptedLetter = this.doSomethingAlready(message, key);
+
+    return encryptedLetter;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
     }
+
+    let yesStartHacking = true;
+
+    let decryptedLetter = this.doSomethingAlready(
+      message,
+      key,
+      yesStartHacking
+    );
+
+    return decryptedLetter;
+  }
 }
 
 module.exports = VigenereCipheringMachine;
