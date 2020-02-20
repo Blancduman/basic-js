edfce0566a6bde7286abd5c4b7cdf2442fd667b8
Ibragim Abbasov <blancduman@gmail.com>
2020-02-20 20:58:01 +0400

    feat: subtask count cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  const catground = [].concat(...backyard);
+  return catground.reduce((cats, cat) => {
+    cats = cat === "^^" ? cats + 1 : cats;
+    return cats;
+  }, 0);
 };
