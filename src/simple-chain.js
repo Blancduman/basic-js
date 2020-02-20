684699fb1fbf90d167413a4ba8cf0ae77842d9dd
Ibragim Abbasov <blancduman@gmail.com>
2020-02-20 21:04:52 +0400

    feat: subtask chain maker done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,37 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (
+      Number.isInteger(position) &&
+      position > 0 &&
+      position < this.chain.length
+    ) {
+      this.chain.splice(position - 1, 1);
+
+      return this;
+    }
+
+    this.chain = [];
+    throw new Error();
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const finishedChain = this.chain.join("~~");
+    this.chain = [];
+
+    return finishedChain;
   }
 };
 
